6608851c3f3c24694ebb5b817752f9b04073555c
plondon <plondon@gm.slc.edu>
2014-07-22 22:23:05 -0400

    move ham funct out of doc ready

--- js/main.js
+++ js/main.js
@@ -1,26 +1,25 @@
+var Hamburger = function(button, menu, overlay) {
+  this.button  = button;
+  this.menu    = menu;
+  this.overlay = overlay;
+}
+
+Hamburger.prototype.toggle = function() {
+  if (this.button.hasClass('active')) {
+    this.button.removeClass('active');
+    this.menu.removeClass('active');
+    this.overlay.removeClass('active');
+  } else {
+    this.button.addClass('active');
+    this.menu.addClass('active');
+    this.overlay.addClass('active');
+  }
+}
+
 $(document).on('ready', function() {
-
-  var button = $('#hamburger');
-  var menu   = $('#menu');
+  var button  = $('#hamburger');
+  var menu    = $('#menu');
   var overlay = $('#overlay');
-
-  var Hamburger = function(button, menu, overlay) {
-    this.button  = button;
-    this.menu    = menu;
-    this.overlay = overlay;
-  }
-
-  Hamburger.prototype.toggle = function() {
-    if (this.button.hasClass('active')) {
-      this.button.removeClass('active');
-      this.menu.removeClass('active');
-      this.overlay.removeClass('active');
-    } else {
-      this.button.addClass('active');
-      this.menu.addClass('active');
-      this.overlay.addClass('active');
-    }
-  }
 
   var ham = new Hamburger(button, menu, overlay)
 
